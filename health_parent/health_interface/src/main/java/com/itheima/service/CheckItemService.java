837e70a1dc65b9229b48cb0513cd33c6b612b23c
mengjianglong <wuan_mjl@163.com>
2020-01-17 20:18:52 +0800

    1.17

---
+++ health_parent/health_interface/src/main/java/com/itheima/service/CheckItemService.java
@@ -0,0 +1,17 @@
+package com.itheima.service;
+
+import java.util.List;
+
+import com.itheima.entity.PageResult;
+import com.itheima.entity.QueryPageBean;
+import com.itheima.pojo.CheckItem;
+
+//服务接口
+public interface CheckItemService {
+    public void add(CheckItem checkItem);
+    public PageResult pageQuery(QueryPageBean queryPageBean);
+    public void deleteById(Integer id);
+    public void edit(CheckItem checkItem);
+    public CheckItem findById(Integer id);
+	public List<CheckItem> findByList();
+}
